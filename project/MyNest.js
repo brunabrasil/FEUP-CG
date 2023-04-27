f4cce1590a1cf58e129803f0e3be440f50281d7d
brunabrasil <up202007191@up.pt>
2023-04-27 23:19:20 +0100

    start of egg

--- project/MyNest.js
+++ project/MyNest.js
@@ -1,6 +1,6 @@
 import {CGFobject} from '../lib/CGF.js';
 /**
-* MyPrism
+* MyNest
 * @constructor
  * @param scene - Reference to MyScene object
  * @param slices - number of divisions around the Y axis

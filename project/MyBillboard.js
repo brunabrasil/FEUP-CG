1a8abe07e3864c0f8cd079fa06f2086ebf1a9e16
lia <liasilva2002@hotmail.com>
2023-05-04 12:17:56 +0100

    terrain done

--- project/MyBillboard.js
+++ project/MyBillboard.js
@@ -1,171 +1,130 @@
-import {CGFobject, CGFappearance, CGFtexture, CGFshader} from '../lib/CGF.js';
-import { MyQuad } from './MyQuad.js';
+// import {CGFobject, CGFappearance, CGFtexture, CGFshader} from '../lib/CGF.js';
+// import { MyQuad } from './MyQuad.js';
 
-// /**
-//  * MyBillboard
-//  * @constructor
-//  * @param scene - Reference to MyScene object
-//  */
-// export class MyBillboard extends CGFobject {
-//     constructor(scene) {
-//         super(scene);
+// export class MyBillboard {
+//     constructor(scene, coords=[0,0,0]) {
+//         this.x = coords[0];
+//         this.y = coords[1];
+//         this.z = coords[2];
 
-//         this.plane = new MyPlane(scene, 30);
-//         // this.suppliesDelivered = 0;
 
-//         // -- Materials -- //
+//         this.quad = new MyQuad(scene);
 //         this.material = new CGFappearance(scene);
-//         this.material.setAmbient(1.0, 1.0, 1.0, 1.0);
-//         this.material.setDiffuse(0.6, 0.6, 0.6, 1.0);
-//         this.material.setSpecular(0.4, 0.4, 0.4, 1.0);
-//         this.material.setShininess(5.0);
-//         this.material.setTextureWrap('REPEAT', 'REPEAT');
-
-//         this.white = new CGFappearance(scene);
-//         this.white.setAmbient(1.0, 1.0, 1.0, 1.0);
-//         this.white.setDiffuse(1.0, 1.0, 1.0, 1.0);
-//         this.white.setSpecular(1.0, 1.0, 1.0, 1.0);
-//         this.white.setShininess(5.0);
-//         this.white.setTextureWrap('REPEAT', 'REPEAT');
-
-//         this.grey = new CGFappearance(scene);
-//         this.grey.setAmbient(0.5, 0.5, 0.5, 1.0);
-//         this.grey.setDiffuse(0.5, 0.5, 0.5, 1.0);
-//         this.grey.setSpecular(0.5, 0.5, 0.5, 1.0);
-//         this.grey.setShininess(5.0);
-//         this.grey.setTextureWrap('REPEAT', 'REPEAT');
-
-
-//         // -- Shaders -- //
-//         this.shader = new CGFshader(scene.gl, "shaders/billboard.vert", "shaders/billboard.frag");
-
-//         // this.shader.setUniformsValues({
-//         //     suppliesDelivered: this.suppliesDelivered / 5.0
-//         // });
+//         this.material.setAmbient(1, 1, 1, 1);
+//         this.material.setDiffuse(1, 1, 1, 1);
+//         this.material.setSpecular(0, 0, 0, 1);
+//         this.material.setShininess(10.0);
+//         this.material.setTexture(new CGFtexture(scene, "images/billboardtree.png"));
+//         this.material.setTextureWrap("REPEAT", "REPEAT");
 //     }
 
-//     // /**
-//     //  * Updates the billboard's bar according to the current number of droped supplies
-//     //  * @param {number} supplies MyScene's nymber of supplies droped
-//     //  */
-//     // update(supplies) {
-//     //     this.suppliesDelivered = supplies;
-//     //     this.shader.setUniformsValues({
-//     //         suppliesDelivered: this.suppliesDelivered / 5.0
-//     //     });
-//     // }
+//     display(x, y, z) {
 
-//     /**
-//      * Displays the billboard in a certain position
-//      */
-//     display() {
-//         // -- Planes -- //
-//         // -- Material -- //
-//         // -- Object Front -- //
-//         this.scene.pushMatrix();
-//         this.scene.translate(0, 3 / 2, 0);
-//         this.scene.scale(2, 1, 1);
-//         this.plane.display();
-//         this.scene.popMatrix();
+//         //let origin = [this.x, this.y, this.z];
 
-//         // -- Material -- //
-//         this.white.apply();
-//         this.scene.pushMatrix();
-//         // -- Object Back -- //
-//         this.scene.translate(0, 3 / 2, 0);
-//         this.scene.scale(2, 1, 1);
-//         this.scene.rotate(Math.PI, 0, 1, 0);
-//         this.plane.display();
-//         this.scene.popMatrix();
+//         //let cameraPosition = this.quad.scene.camera.position;
+//         //let toCamera = [cameraPosition[0] - origin[0], cameraPosition[1] - origin[1], cameraPosition[2] - origin[2]];
+//         //toCamera = CGFvec3.normalize(toCamera);
+//         //let quadNormal = [0, 1, 0];
+//         //let angle = Math.acos(CGFvec3.dot(toCamera, quadNormal));
+//         //let axis = CGFvec3.cross(quadNormal, toCamera);
+//         //axis = CGFvec3.normalize(axis);
 
-//         // -- Posts (Height = 1) -- //
-//         // -- Material -- //
-//         this.grey.apply();
-//         // -- Objects Front -- //
-//         this.scene.pushMatrix();
-//         this.scene.translate(-15 / 16, 1 / 2, 0);
-//         this.scene.scale(1 / 8, 1, 1);
-//         this.plane.display();
-//         this.scene.popMatrix();
-//         this.scene.pushMatrix();
-//         this.scene.translate(15 / 16, 1 / 2, 0);
-//         this.scene.scale(1 / 8, 1, 1);
-//         this.plane.display();
-//         this.scene.popMatrix();
-//         // -- Objects Back -- //
-//         this.scene.pushMatrix();
-//         this.scene.translate(-15 / 16, 1 / 2, 0);
-//         this.scene.scale(1 / 8, 1, 1);
-//         this.scene.rotate(Math.PI, 0, 1, 0);
-//         this.plane.display();
-//         this.scene.popMatrix();
-//         this.scene.pushMatrix();
-//         this.scene.translate(15 / 16, 1 / 2, 0);
-//         this.scene.scale(1 / 8, 1, 1);
-//         this.scene.rotate(Math.PI, 0, 1, 0);
-//         this.plane.display();
-//         this.scene.popMatrix();
+//         //let modelViewMatrix = mat4.create();
+//         //mat4.translate(modelViewMatrix, modelViewMatrix, [x, y, z]);
+//         //mat4.rotate(modelViewMatrix, modelViewMatrix, angle, axis);
+//         //mat4.rotate(modelViewMatrix, modelViewMatrix, -Math.PI/2, [1, 0, 0]);
+//         //this.quad.scene.setActiveShader(this.quad.scene.textureShader);
+//         //this.material.apply();
+//         //this.quad.display();
+//         //this.quad.scene.setActiveShader(this.quad.scene.defaultShader);
+        
+//         let cameraPosition = vec3.fromValues(x, y, z);
+//         let cameraToQuad = vec3.create();
+//         cam.subtract(cameraToQuad, cameraPosition, quadPosition);
+//         vec3.normalize(cameraToQuad, cameraToQuad);
 
-//         // -- Progress Bar -- //
-//         // -- Material -- //
+//         const horizontalCameraToQuad = vec3.fromValues(cameraToQuad[0], 0, cameraToQuad[2]);
+//         vec3.normalize(horizontalCameraToQuad, horizontalCameraToQuad);
+
+//         const angle = Math.acos(vec3.dot(cameraToQuad, vec3.fromValues(0, 1, 0)));
+//         vec3.cross(rotationAxis, vec3.fromValues(0, 1, 0), cameraToQuad);
+
+//         this.quad.scene.pushMatrix();
+//         this.quad.scene.translate(x, y, z);
+//         this.quad.scene.rotate(angle, 0, 1, 0);
+//         //this.quad.scene.rotate(-Math.PI / 2, 1, 0, 0); // para ficar na vertical
 //         this.material.apply();
-//         // -- Object -- //
-//         this.scene.pushMatrix();
-//         this.scene.setActiveShader(this.shader);
-//         this.scene.translate(0, 3 / 2, 0.005);
-//         this.scene.scale(3 / 2, 1 / 5, 1);
-//         this.plane.display();
-//         this.scene.popMatrix();
-//         // -- Shader is reset in MyScene -- //
+//         this.quad.display();
+//         this.quad.scene.popMatrix();
 //     }
 
-//     /**
-//      * Enables visualization of Object's normals
-//      */
-//     enableNormalViz() {
-//         this.plane.enableNormalViz()
-//     }
-//     /**
-//      * Disables visualization of Object's normals
-//      */
-//     disableNormalViz() {
-//         this.plane.disableNormalViz()
+//     update() {
+
+//         let camx = this.scene.camera.position[0];
+//         let camy = this.scene.camera.position[1];
+//         let camz = this.scene.camera.position[2];
+
+
 //     }
 // }
 
+import { CGFobject, CGFappearance, CGFtexture, CGFshader } from '../lib/CGF.js';
+import { MyQuad } from './MyQuad.js';
+
 export class MyBillboard {
-    constructor(scene) {
+    constructor(scene, coords = [0, 0, 0]) {
+        //super(scene);
+
+        this.x = coords[0];
+        this.y = coords[1];
+        this.z = coords[2];
+
         this.quad = new MyQuad(scene);
         this.material = new CGFappearance(scene);
         this.material.setAmbient(1, 1, 1, 1);
         this.material.setDiffuse(1, 1, 1, 1);
         this.material.setSpecular(0, 0, 0, 1);
         this.material.setShininess(10.0);
-        this.material.setTexture(new CGFtexture(scene, "textures/billboardtree.png"));
-        this.material.setTextureWrap("REPEAT", "REPEAT");
+        this.material.setTexture(new CGFtexture(scene, 'images/billboardtree.png'));
+        this.material.setTextureWrap('REPEAT', 'REPEAT');
+
+         
     }
 
     display(x, y, z) {
-        this.material.apply();
-
-        const cameraPosition = this.quad.scene.camera.position;
-        const quadPosition = vec3.fromValues(x, y, z);
-        const cameraToQuad = vec3.create();
-        vec3.subtract(cameraToQuad, cameraPosition, quadPosition);
+        this.scene.pushMatrix();
+        let camPosition = this.scene.camera.position;  
+        let quadPosition = [this.x, 0, this.z];
+        let cameraToQuad = [cameraPos[0]-quadPosition[0],0,cameraPos[2]-quadPosition[2]];
         vec3.normalize(cameraToQuad, cameraToQuad);
 
-        const horizontalCameraToQuad = vec3.fromValues(cameraToQuad[0], 0, cameraToQuad[2]);
-        vec3.normalize(horizontalCameraToQuad, horizontalCameraToQuad);
+        let billboardNormal = vec3.fromValues(this.billboard.normals[0], this.billboard.normals[1], this.billboard.normals[2]);
+        let rotationAxis = vec3.create();
+        vec3.cross(rotationAxis, billboardNormal, cameraToQuad);
 
-        const angle = Math.acos(vec3.dot(cameraToQuad, vec3.fromValues(0, 1, 0)));
-        const rotationAxis = vec3.create();
-        vec3.cross(rotationAxis, vec3.fromValues(0, 1, 0), cameraToQuad);
+        let angle = Math.acos(vec3.dot(billboardNormal, cameraToQuad));
 
-        this.quad.scene.pushMatrix();
-        this.quad.scene.translate(x, y, z);
-        this.quad.scene.rotate(angle, rotationAxis[0], rotationAxis[1], rotationAxis[2]);
-        this.quad.scene.rotate(-Math.PI / 2, 1, 0, 0); // para ficar na vertical
+        this.scene.translate(this.x, this.y, this.z);
+        this.scene.rotate(angle, rotationAxis[0], rotationAxis[1], rotationAxis[2]);
+        
+        this.scene.translate(this.x, this.y, this.z);
+
+        this.material.apply();
         this.quad.display();
-        this.quad.scene.popMatrix();
+        this.scene.popMatrix();
+    }
+
+    update() {
+        
+    }
+
+    enableNormalViz() {
+        this.quad.enableNormalViz();
+    }
+
+    disableNormalViz() {
+        this.quad.disableNormalViz();
     }
 }
+
--- project/MyTerrain.js
+++ project/MyTerrain.js
@@ -6,50 +6,45 @@ import { MyPlane } from './MyPlane.js';
  * @constructor
  * @param scene - Reference to MyScene object
  */
+
 export class MyTerrain extends CGFobject {
-	constructor(scene) {
-        super(scene);
 
-        this.plane = new MyPlane(scene, 30);
+constructor(scene) {
+    super(scene);
+    this.terrain = new MyPlane(scene, 30);
 
-        this.initMaterials(scene);
-        this.initShaders(scene);
-	}
+    this.initMaterials(scene);
+    this.initShaders(scene);
+  }
 
-    initShaders(scene) {
-	this.terrainShader = new CGFshader(scene.gl, "shaders/terrain.vert", "shaders/terrain.frag");
-        this.terrainShader.setUniformsValues({ terrainMap: 1, offset: 0.2, multiplier: 0.3 });
-    }
+initShaders(scene) {
 
+    this.shader = new CGFshader(scene.gl, "./shaders/terrain.vert", "./shaders/terrain.frag");
+    this.shader.setUniformsValues({ bumpMap: 1, offset: 0.2, multiplier: 0.3, altimetry: 2});
+}
 
-    initMaterials(scene) { 
+initMaterials(scene) {
+    this.bumpMap = new CGFtexture(this.scene, "images/heightmap.jpg");
+    this.altimetry = new CGFtexture(this.scene, "images/altimetry.png");
+    this.appearance = new CGFappearance(scene);
+    this.appearance.loadTexture("images/terrain.jpg");
+    this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
-	this.textureHeightmap = new CGFtexture(scene, "images/heightmap.jpg");
-        this.altimetry = new CGFtexture(scene, "images/altimetry.png");
+}
 
-        this.appearance = new CGFappearance(scene);
-        this.appearance.loadTexture("images/terrain.jpg");
-        this.appearance.setTextureWrap('REPEAT', 'REPEAT');
-        // this.appearance.setTexture(this.textureTerrain);
-        // this.appearance.setTextureWrap('REPEAT', 'REPEAT');
-
-    }
-
-    display() {
-        this.scene.setActiveShader(this.terrainShader);
-        this.textureHeightmap.bind(1);
-        this.altimetry.bind(2);
-
-        this.scene.pushMatrix();
-        this.appearance.apply();
-        this.scene.translate(0, -30, 0);
-        this.scene.rotate(-Math.PI/2, 1, 0, 0);
-        this.scene.scale(400, 400, 200);
-        this.plane.display();
-        this.scene.popMatrix();
-
-        this.scene.setActiveShader(this.scene.defaultShader);
-    }
-
+display() {
+    this.scene.setActiveShader(this.shader);
+    this.bumpMap.bind(1);
+    this.altimetry.bind(2);
     
+    this.scene.pushMatrix();
+    this.appearance.apply();
+    this.scene.translate(0, -30, 0);
+    this.scene.rotate(-Math.PI/2, 1, 0, 0);
+    this.scene.scale(400, 400, 200);
+    this.terrain.display();
+    this.scene.popMatrix();
+
+    this.scene.setActiveShader(this.scene.defaultShader);
+}
 }

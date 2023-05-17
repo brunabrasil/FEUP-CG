c5942fce4546c947d5fe9f5bc73aa44cf9f63198
lia <liasilva2002@hotmail.com>
2023-05-17 16:14:39 +0100

    trees done

--- project/MyBillboard.js
+++ project/MyBillboard.js
@@ -13,7 +13,7 @@ export class MyBillboard extends CGFobject {
     initMaterials(scene) {
 
         this.treeText = new CGFappearance(scene);
-        const textures = ["images/billboardtree4.png", "images/billboardtree2.png", "images/billboardtree3.png"];
+        const textures = ["images/billboardtree.png", "images/billboardtree2.png", "images/billboardtree3.png"];
         const index = Math.floor(Math.random() * textures.length);
         this.treeText.loadTexture(textures[index]);
         this.treeText.setTextureWrap('REPEAT', 'REPEAT');
